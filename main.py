ac22541040abf152a5c9befce5c857925130e8f1
carl <carl@ubuntu.com>
2023-05-08 18:00:02 +0000

    new languages

--- main.py
+++ main.py
@@ -38,15 +38,22 @@ async def translate(request: Request):
 
 
 @app.get('/result')
-async def get_result(request: Request, text: str, source_lang: str, target_lang: str):
+async def get_result(request: Request, text: str, target_lang: str, source_lang: str | None = None):
     key = f'tranlation.{source_lang}.{target_lang}.{text}'
     cached = await redis.get(key)
     if cached is not None:
         data = json.loads(cached)
+        if source_lang is None or len(source_lang) == 0:
+            source_lang = data['translations'][0]['detected_source_language']
         return templates.TemplateResponse('index.html', {'request': request, 'input': text, 'result': data['translations'][0]['text'], 'target_lang': target_lang, 'source_lang': source_lang})
 
-    payload = {'text': text, 'source_lang': source_lang, 'target_lang': target_lang}
+    payload = {'text': text, 'target_lang': target_lang}
+    if source_lang != '' and source_lang is not None:
+        payload['source_lang'] = source_lang
+
     async with session.post('https://api-free.deepl.com/v2/translate', data=payload) as res:
         data = await res.json()
+        if source_lang is None or len(source_lang) == 0:
+            source_lang = data['translations'][0]['detected_source_language']
         await redis.set(key, json.dumps(data))
         return templates.TemplateResponse('index.html', {'request': request, 'input': text, 'result': data['translations'][0]['text'], 'target_lang': target_lang, 'source_lang': source_lang})
